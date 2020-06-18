bf2ddf74ce7202d31a1a6178c32fc39dcca1ead0
Jochen Schmücking <jochen.schmuecking@prodyna.com>
2020-06-19 01:57:57 +0200

    added angular material + basic toolbar

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  exports: [MatToolbarModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
